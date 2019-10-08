01c5273f7151151d695b96c2c855350a7288d4c2
Iftikhar Yar Khan <34522152+iftikharyk@users.noreply.github.com>
2019-10-08 11:35:04 +0500

    added api of iex cloud

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const exshbs = require('express-handlebars');
+const request = require('request');
 
 const PORT = process.env.PORT || 5000;
 
@@ -9,15 +10,36 @@ const PORT = process.env.PORT || 5000;
 app.engine('handlebars', exshbs());
 app.set('view engine', 'handlebars');
 
+// Api key (public) pk_dbab12835ccf488daff61bba25814ee1
+function callApi() {
+    request('https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_dbab12835ccf488daff61bba25814ee1', { json: true }, (err, res, body) => {
+        if (err) {
+            return console.log(err);
+        }
+    
+        if (res.statusCode === 200) {
+            console.log(body);
+            return body;
+        } else {
+            console.log(res.statusCode);
+        }
+    });
+}
+
+
 // Set handlebar routes
 app.get('/', function(req, res) {
+    const api = callApi();
     res.render('home', {
-        stuff: "This is stuff"
+        stock: api
     });
 });
 
 app.get('/about.html', function(req, res) {
-    res.render('about');
+    const otherstuff = "This is other stuff";
+    res.render('about', {
+        stuff: otherstuff
+    });
 });
 
 // Routes (to public)
