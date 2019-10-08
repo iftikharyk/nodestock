e6e5951fb3709fb888dcb99dabba3cd5a62db10a
Iftikhar Yar Khan <34522152+iftikharyk@users.noreply.github.com>
2019-10-08 16:09:52 +0500

    added dynamic form search api to get the values

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ const app = express();
 const path = require('path');
 const exshbs = require('express-handlebars');
 const request = require('request');
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 5000;
 
-// Set handlebars Middleware
-app.engine('handlebars', exshbs());
-app.set('view engine', 'handlebars');
+// Use body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
 
 // Api key (public) pk_dbab12835ccf488daff61bba25814ee1
-function callApi(finishedApi) {
-    request('https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_dbab12835ccf488daff61bba25814ee1', { json: true }, (err, res, body) => {
+function callApi(finishedApi, ticker) {
+    request('https://cloud.iexapis.com/stable/stock/'+ticker+'/quote?token=pk_dbab12835ccf488daff61bba25814ee1', { json: true }, (err, res, body) => {
         if (err) { return console.log(err); }
     
         if (res.statusCode === 200) {
@@ -24,7 +24,11 @@ function callApi(finishedApi) {
 }
 
 
-// Set handlebar routes
+// Set handlebars Middleware
+app.engine('handlebars', exshbs());
+app.set('view engine', 'handlebars');
+
+// Set handlebar index GET route
 app.get('/', function(req, res) {
 
     // Adding callback function
@@ -32,10 +36,25 @@ app.get('/', function(req, res) {
         res.render('home', {
             stock: doneApi
         });
-    });
+    }, "fb");
 
 });
 
+// Set handlebar index POST route
+app.post('/', function(req, res) {
+
+    // Adding callback function
+    callApi(function(doneApi) {
+        // posted_stuff = req.body.stock_ticker;
+        res.render('home', {
+            stock: doneApi,
+            // search_stuff: posted_stuff
+        });
+    }, req.body.stock_ticker);
+
+});
+
+// Set handlebars about route
 app.get('/about.html', function(req, res) {
     const otherstuff = "This is other stuff";
     res.render('about', {
